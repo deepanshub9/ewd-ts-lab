4bcc99ceba151a7b11ea2dbefa5169c44ed9a3f2
Deepanshu <kukukumar1996@gmail.com>
2025-01-22 00:48:45 +0000

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,30 +1,22 @@
 import { Friend, Colleague, ColleagueHistory } from './myTypes';
 
 
-
-
-const friend1 = {
-  name: "Paul Fleming",
-  phone: "087-12345",
-  age: 25,
-};
-
-const friend2 = {
-  name: "Jane Costello",
-  phone: "086-12345",
-  age: 31,
-};
-
+const friend1 : Friend = {
+    name: "Paul Fleming",
+    phone: "087-12345",
+    age: 25,
+    dob: new Date("1998-11-20") ,
+  };
+  
+  const friend2 : Friend = {
+    name: "Jane Costello",
+    phone: "086--12345",
+    age: 31,
+    interests: ['Music', 'Sport']
+  };
+  
 
 // console.log(friends[1]);
-
-
-
-//   -------------------
-
-
-
-
 
 const colleague1 = {
   name: "Ralph Graham",
